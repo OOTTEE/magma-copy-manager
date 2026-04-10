09139b19020424e8832ddd276164f0f67a3a32b8
Javier (OTE) <javier.ote@protonmail.com>
2026-04-10 10:04:16 +0200

    chore: remove obsolete test scripts and project files while updating gitignore patterns

--- printRoutes.ts
+++
@@ -1,6 +0,0 @@
-import { buildApp } from './core/app';
-const app = buildApp();
-app.ready().then(() => {
-  console.log('--- ROUTES ---');
-  console.log(app.printRoutes());
-}).catch(console.error);
